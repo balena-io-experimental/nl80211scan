14f0c1e8c9d40389ab6850301dc459a2f2b255dd
Zahari Petkov <zahari@balena.io>
2022-01-27 12:29:48 +0200

    Move interface related code to a new module
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 mod enums;
+mod interface;
 
 #[allow(dead_code, non_upper_case_globals, non_camel_case_types)]
 mod consts;
 
-use std::convert::{TryFrom, TryInto};
 use std::hash::Hash;
 use std::io::Cursor;
 use std::io::Read;
 
 use anyhow::{bail, Context, Result};
 
-use macaddr::MacAddr6;
+use byteorder::ReadBytesExt;
 
+use neli::attr::Attribute;
 use neli::consts::nl::{NlmF, NlmFFlags, Nlmsg};
 use neli::consts::socket::NlFamily;
 use neli::consts::MAX_NL_LENGTH;
@@ -21,9 +22,8 @@ use neli::socket::tokio::NlSocket;
 use neli::socket::NlSocketHandle;
 use neli::types::{Buffer, GenlBuffer};
 
-use enums::{Nl80211Attr, Nl80211Bss, Nl80211Cmd};
-
-use byteorder::ReadBytesExt;
+use crate::enums::{Nl80211Attr, Nl80211Bss, Nl80211Cmd};
+use crate::interface::Interface;
 
 const NL80211_FAMILY_NAME: &str = "nl80211";
 const SCAN_MULTICAST_NAME: &str = "scan";
@@ -34,84 +34,6 @@ pub struct Station {
     pub ssid: String,
     pub quality: u8,
 }
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum InterfaceType {
-    Unspecified = 0,
-    Adhoc,
-    Station,
-    AP,
-    APVlan,
-    WDS,
-    Monitor,
-    MeshPoint,
-    P2PClient,
-    P2PGo,
-    P2PDevice,
-    Ocb,
-    Nan,
-}
-
-impl From<::std::os::raw::c_uint> for InterfaceType {
-    fn from(orig: ::std::os::raw::c_uint) -> Self {
-        match orig {
-            consts::NL80211_IFTYPE_UNSPECIFIED => InterfaceType::Unspecified,
-            consts::NL80211_IFTYPE_ADHOC => InterfaceType::Adhoc,
-            consts::NL80211_IFTYPE_STATION => InterfaceType::Station,
-            consts::NL80211_IFTYPE_AP => InterfaceType::AP,
-            consts::NL80211_IFTYPE_AP_VLAN => InterfaceType::APVlan,
-            consts::NL80211_IFTYPE_WDS => InterfaceType::WDS,
-            consts::NL80211_IFTYPE_MONITOR => InterfaceType::Monitor,
-            consts::NL80211_IFTYPE_MESH_POINT => InterfaceType::MeshPoint,
-            consts::NL80211_IFTYPE_P2P_CLIENT => InterfaceType::P2PClient,
-            consts::NL80211_IFTYPE_P2P_GO => InterfaceType::P2PGo,
-            consts::NL80211_IFTYPE_P2P_DEVICE => InterfaceType::P2PDevice,
-            consts::NL80211_IFTYPE_OCB => InterfaceType::Ocb,
-            consts::NL80211_IFTYPE_NAN => InterfaceType::Nan,
-            _ => InterfaceType::Unspecified,
-        }
-    }
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Clone)]
-pub struct Interface {
-    name: String,
-    index: u32,
-    iftype: InterfaceType,
-    wiphy: u32,
-    wdev: u64,
-    mac_address: MacAddr6,
-}
-
-impl TryFrom<&Genlmsghdr<Nl80211Cmd, Nl80211Attr>> for Interface {
-    type Error = anyhow::Error;
-
-    fn try_from(payload: &Genlmsghdr<Nl80211Cmd, Nl80211Attr>) -> Result<Self, Self::Error> {
-        let attrs = payload.get_attr_handle();
-        let name = attrs.get_attr_payload_as_with_len(Nl80211Attr::Ifname)?;
-        let index = attrs.get_attr_payload_as(Nl80211Attr::Ifindex)?;
-        let iftype = attrs
-            .get_attr_payload_as::<u32>(Nl80211Attr::Iftype)?
-            .into();
-        let wiphy = attrs.get_attr_payload_as(Nl80211Attr::Wiphy)?;
-        let wdev = attrs.get_attr_payload_as(Nl80211Attr::Wdev)?;
-        let mac_bytes: [u8; 6] = attrs
-            .get_attr_payload_as_with_len::<&[u8]>(Nl80211Attr::Mac)?
-            .try_into()?;
-        let mac_address = mac_bytes.into();
-        Ok(Interface {
-            name,
-            index,
-            iftype,
-            wiphy,
-            wdev,
-            mac_address,
-        })
-    }
-}
-
-use neli::attr::Attribute;
 
 pub async fn scan(interface: &str) -> Result<Vec<Station>> {
     let mut socket_handle = NlSocketHandle::connect(NlFamily::Generic, None, &[])
